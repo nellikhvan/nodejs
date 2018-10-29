91f177b9aa23bb6ca83b5984026ea01ae807752b
Nelli <nellikhvan@gmail.com>
2018-10-29 17:35:19 +0100

    Fix update, delete

--- week_03/src/router.js
+++ week_03/src/router.js
@@ -3,30 +3,31 @@
 const Router = require('koa-router')
 const dogs = require('./dogs')
 const { validate } = require('./utils/validation')
+
 const schema = {
-    type: 'Object',
-    required: true,
-    properties: {
-        id: {
-            type: 'integer',
-            required: true,
-        },
-        name: {
-            type: 'string',
-            required: true,
-        },
-        breed: {
-            type: 'string',
-            required: true,
-        },
-        birthYear: {
-            type: 'number',
-        },
-        photo: {
-            type: 'string',
-            format: 'url',
-        },
+  type: 'Object',
+  required: true,
+  properties: {
+    id: {
+      type: 'integer',
+      required: true,
     },
+    name: {
+      type: 'string',
+      required: true,
+    },
+    breed: {
+      type: 'string',
+      required: true,
+    },
+    birthYear: {
+      type: 'number',
+    },
+    photo: {
+      type: 'string',
+      format: 'url',
+    },
+  },
 }
 
 const router = new Router()
@@ -48,64 +49,49 @@ router.get('/dogs/:id', ctx => {
 
 // create
 router.post('/dogs', ctx => {
-
-    const validation = validate(ctx.request.body, schema)
-    if (!validation.valid) {
-        ctx.status = 400
-        ctx.body = {
-            errors: validation.errors,
-        }
-        return
+  const validation = validate(ctx.request.body, schema)
+  if (!validation.valid) {
+    ctx.status = 400
+    ctx.body = {
+      errors: validation.errors,
     }
+    return
+  }
   dogs.push(ctx.request.body)
 
   ctx.body = dogs
 })
 
-//update
+// update
 router.put('/dogs/:id', ctx => {
+    const dog = dogs.findIndex(item => item.id === Number(ctx.params.id))
     const validation = validate(ctx.request.body, schema)
+    if (!dog) {
+        ctx.status = 404
+        return
+    }
     if (!validation.valid) {
         ctx.status = 400
         ctx.body = {
             errors: validation.errors,
         }
         return
-    } else {
-        const dog = dogs.find(item => item.id === Number(ctx.params.id))
-
-        if (!dog) {
-            ctx.status = 404
-            return
-        }
-        dogs[dog.id] = {
-            id: ctx.params.id,
-            name: ctx.request.name,
-            breed: ctx.request.breed,
-            birthYear: ctx.request.birthYear,
-            photo: ctx.request.photo
-        }
-        ctx.body = dogs
     }
+    dogs.splice(dog, 1, ctx.request.body)
+    ctx.body = dogs
+
 })
 
-//delete
+// delete
 router.delete('/dogs/:id', ctx => {
-    const dog = dogs.find(item => item.id === Number(ctx.params.id))
+  const dog = dogs.findIndex(item => item.id === Number(ctx.params.id))
 
-    if (!dog) {
-        ctx.status = 404
-        return
-    }
-
-    const id = dog.toString().indexOf(ctx.params.id)
-    if (id !== -1) {
-        delete dogs[id]
-        ctx.body = dogs
-    } else {
-        ctx.status = 404
-        return
-    }
+  if (!dog) {
+    ctx.status = 404
+    return
+  }
+    dogs.splice(dog, 1)
+    ctx.body = dogs
 
 })
 
