4517cc6a4f16452a1b635b9e4122a196ef39bad5
Nelli Khvan <n.khvan@devcaz.com>
2018-11-07 10:10:34 +0100

    Add week 5 for database

---
+++ week_05/src/config/env/local.js
@@ -0,0 +1,6 @@
+
+'use strict'
+
+module.exports = {
+    hostname: 'http://localhost:3000',
+}
---
+++ week_05/src/controllers/users.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const { validate } = require('../validations')
+const operations = require('../operations/users')
+const schema = require('../validations/schemas/users')
+
+async function login(ctx) {
+    const input = {
+        email: ctx.request.body.username,
+        password: ctx.request.body.password,
+    }
+    validate(schema.login, input)
+    ctx.body = await operations.login(input)
+}
+
+async function signUp(ctx) {
+    const input = {
+        name: ctx.request.body.name,
+        email: ctx.request.body.email,
+        password: ctx.request.body.password,
+    }
+    validate(schema.signUp, input)
+    const user = await operations.signUp(input)
+    ctx.status = 201
+    ctx.body = user
+}
+
+module.exports = {
+    login,
+    signUp,
+}
